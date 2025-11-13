3a524871113d01df4b2816bf87ca51e10d2d3952
OgliariNatan <natanogliari@gmail.com>
2025-11-13 09:52:30 -0300

    atualizando relatorio

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #Script para compilação de documentos LaTeX com BibTeX
 # Autor: OgliariNatan
 # Atualizado em: 20250923
-pdflatex main.tex && bibtex main.aux && pdflatex main.tex
+pdflatex main.tex && bibtex main.aux && pdflatex main.tex && pdflatex main.tex
 
 
 # Roda com erros
-#pdflatex main.tex ; bibtex main.aux ; pdflatex main.tex
+#pdflatex main.tex ; bibtex main.aux ; pdflatex main.tex ; pdflatex main.tex
